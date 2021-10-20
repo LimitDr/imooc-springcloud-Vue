7cca5ae29fbedd0a131ebb63db8355fd2750364f
lyleli <1546296848@qq.m>
2021-10-20 13:59:33 +0800

    1. 将system配置为最简单的springboot项目

---
+++ system/src/main/java/com/Lyle/system/SystemApplication.java
@@ -0,0 +1,20 @@
+package com.Lyle.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class SystemApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SystemApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！");
+		LOG.info("System地址：\thttp://localhost:{}", env.getProperty("server.port"));
+	}
+
+}
