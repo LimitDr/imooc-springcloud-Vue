0b538d47c640232de0c35d3d0ffaea10eb37324d
lyleli <1546296848@qq.m>
2021-10-20 13:42:54 +0800

    1. 解决注册中心服务启动失败的问题 2. 优化启动日志显示

--- eureka/src/main/java/com/Lyle/course/EurekaApplication.java
+++ eureka/src/main/java/com/Lyle/course/EurekaApplication.java
@@ -1,13 +1,23 @@
 package com.Lyle.course;
 
+import freemarker.core.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！");
+		LOG.info("Eureka地址：\thttp://localhost:{}", env.getProperty("server.port"));
 	}
 
 }
