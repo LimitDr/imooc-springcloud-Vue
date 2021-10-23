355320848b688b755f35228fe3bf17cc87288d02
lyleli <1546296848@qq.m>
2021-10-23 10:39:04 +0800

    1. 增加了大章表sql 并生成持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,9 +1,10 @@
-drop table if exists `test`;
-create table `test`(
+drop table if exists `chapter`;
+create table `chapter`(
     `id` char(8) not null default '' comment 'id',
+    `course_id` char(8) comment '课程id',
     `name` varchar(255) comment '名称',
     primary key (`id`)
-) engine = innodb default charset = utf8mb4 comment '测试';
+) engine = innodb default charset = utf8mb4 comment '大章';
 
 insert into `test` (`id`, `name`) values (1, '测试');
 insert into `test` (`id`, `name`) values (2, '测试2');
