80c7b781c3652984c3a1878d1ba852f45c41279d
lyleli <1546296848@qq.m>
2021-10-23 10:29:47 +0800

    1. 增加了business模块

---
+++ business/src/main/java/com/Lyle/business/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.Lyle.business.controller;
+
+import com.Lyle.server.domain.Test;
+import com.Lyle.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return testService.list();
+    }
+}
