201083f14cd3c2540dca2be0d1c532e72c1b1570
lyleli <1546296848@qq.m>
2021-10-25 17:44:28 +0800

    1. 使用gateway路由转发实现了9000/business的路由转发

--- gateway/src/main/java/com/Lyle/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/Lyle/gateway/GatewayApplication.java
@@ -5,7 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,5 +24,4 @@ public class GatewayApplication {
 		LOG.info("启动成功！");
 		LOG.info("Gateway地址：\thttp://localhost:{}", env.getProperty("server.port"));
 	}
-
 }
